a7fce3a93151abe27f404653bb9faf4003cfdcd0
Adi Juanito Taklal <aditaklal@gmail.com>
2023-12-18 18:45:30 +0800

    Coba2

---
+++ webSITE/webSITE.Models/Mahasiswa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webSITE.Models
+{
+    public class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string NamaLengkap { get; set; }
+        public string NamaPanggilan { get; set; }
+        public DateTime TanggalLahir { get; set; }
+        public int TahunMasuk { get; set; }
+        public JenisKelamin JenisKelamin { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhotoPath { get; set; }
+        public string StrJenisKelamin { 
+            get 
+            {
+                if (JenisKelamin == JenisKelamin.LakiLaki)
+                    return "Laki - Laki";
+                else
+                    return "Perempuan";
+            } 
+        }
+    }
+}
---
+++ webSITE/webSITE/Controllers/MahasiswaController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webSITE.Models;
+
+namespace webSITE.Controllers
+{
+    public class MahasiswaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var listMahasiswa = new List<Mahasiswa>()
+            {
+                new Mahasiswa()
+                {
+                    Nim = "2206080051",
+                    NamaLengkap = "Adi Juanito Taklal",
+                    NamaPanggilan = "Adi",
+                    JenisKelamin = JenisKelamin.LakiLaki,
+                    TahunMasuk = 2022,
+                    TanggalLahir = new DateTime(2004, 02, 29),
+                    PhotoPath = "/img/contoh.jpeg"
+                },
+                new Mahasiswa()
+                {
+                    Nim = "2206080011",
+                    NamaLengkap = "Oswaldus Putra Fernando",
+                    NamaPanggilan = "Fernand",
+                    JenisKelamin = JenisKelamin.LakiLaki,
+                    TahunMasuk = 2022,
+                    TanggalLahir = new DateTime(2004, 02, 29),
+                    PhotoPath = "/img/contoh.jpeg"
+                },
+                new Mahasiswa()
+                {
+                    Nim = "2206080021",
+                    NamaLengkap = "Albertliano Tapatab",
+                    NamaPanggilan = "Al",
+                    JenisKelamin = JenisKelamin.LakiLaki,
+                    TahunMasuk = 2022,
+                    TanggalLahir = new DateTime(2004, 02, 29),
+                    PhotoPath = "/img/contoh.jpeg"
+                }
+            };
+            return View(listMahasiswa);
+        }
+    }
+}
