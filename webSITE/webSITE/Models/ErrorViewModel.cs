9e3c02a3360b900342db95c32d082587dbc10a5f
Lavitzz <allgaming1185@gmail.com>
2023-12-18 17:40:23 +0800

    webSITE

---
+++ webSITE/webSITE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webSITE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
