1e3824c090f683974bcb3bbf44f2eb3bd5199b67
Putra Fernando <Fernandputra14@gmail.com>
2024-01-07 03:21:23 +0800

    Navbar HomePage...Masih Pemanasan :(

--- webSITE/webSITE/wwwroot/js/site.js
+++ webSITE/webSITE/wwwroot/js/site.js
@@ -1,4 +1,39 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
 
-// Write your JavaScript code.
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open")
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line")
+});
+
+navLinks.addEventListener("click", () => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+    origin: "bottom",
+    distance: "50px",
+    duration: 1000,
+};
+
+//header container
+ScrollReveal().reveal(".header_content p", {
+    ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header_content h1", {
+    ...scrollRevealOption,
+    delay:500
+});
+
+ScrollReveal().reveal(".header_content .header_btn", {
+    ...scrollRevealOption,
+    delay: 1000
+});
+
+
+
