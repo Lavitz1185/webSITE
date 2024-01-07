57713bca7a009c06d54fc4a967e61559f9bc0cc1
Putra Fernando <Fernandputra14@gmail.com>
2024-01-07 23:26:05 +0800

    Homepage Responsive (Belum Lengkap di data Pengurus)

--- webSITE/webSITE/wwwroot/js/site.js
+++ webSITE/webSITE/wwwroot/js/site.js
@@ -1,39 +1,12 @@
-﻿const menuBtn = document.getElementById("menu-btn");
-const navLinks = document.getElementById("nav-links");
-const menuBtnIcon = menuBtn.querySelector("i");
+﻿
+$(document).ready(function () {
+    $('#menu-btn').click(function() {
+        $('nav .navigation ul').addClass('active')
+    });
 
-menuBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
-
-    const isOpen = navLinks.classList.contains("open")
-    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line")
+    $('#menu-close').click(function() {
+        $('nav .navigation ul').removeClass('active')
+    });
 });
-
-navLinks.addEventListener("click", () => {
-    navLinks.classList.remove("open");
-    menuBtnIcon.setAttribute("class", "ri-menu-line");
-});
-
-const scrollRevealOption = {
-    origin: "bottom",
-    distance: "50px",
-    duration: 1000,
-};
-
-//header container
-ScrollReveal().reveal(".header_content p", {
-    ...scrollRevealOption,
-});
-
-ScrollReveal().reveal(".header_content h1", {
-    ...scrollRevealOption,
-    delay:500
-});
-
-ScrollReveal().reveal(".header_content .header_btn", {
-    ...scrollRevealOption,
-    delay: 1000
-});
-
 
 
