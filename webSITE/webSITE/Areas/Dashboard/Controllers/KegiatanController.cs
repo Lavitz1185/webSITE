4440f7191e3d9003793ac953af18050134eacff7
Adi Juanito Taklal <aditaklal@gmail.com>
2024-01-21 18:39:42 +0800

    Tambah Kegiatan Index di dashboard

---
+++ webSITE/webSITE/Areas/Dashboard/Controllers/KegiatanController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using webSITE.Repositori.Interface;
+
+namespace webSITE.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    [Authorize(Roles = "Admin, ADMIN")]
+    public class KegiatanController : Controller
+    {
+        private readonly IRepositoriKegiatan _repositoriKegiatan;
+        private readonly IRepositoriPesertaKegiatan _pesertaKegiatan;
+        private readonly IRepositoriMahasiswa _repositoriMahasiswa;
+
+        public KegiatanController(
+            IRepositoriKegiatan repositoriKegiatan, 
+            IRepositoriPesertaKegiatan pesertaKegiatan, 
+            IRepositoriMahasiswa repositoriMahasiswa)
+        {
+            _repositoriKegiatan = repositoriKegiatan;
+            _pesertaKegiatan = pesertaKegiatan;
+            _repositoriMahasiswa = repositoriMahasiswa;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var listKegiatan = (await _repositoriKegiatan.GetAll()).ToList();
+
+            return View(listKegiatan);
+        }
+    }
+}
