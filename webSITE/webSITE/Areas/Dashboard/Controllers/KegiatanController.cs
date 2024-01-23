4a26b1a71b73993dd9b8cd48272f406bbeabf7ae
Adi Juanito Taklal <aditaklal@gmail.com>
2024-01-23 14:31:58 +0800

    Tambah detail di kegiatan

--- webSITE/webSITE/Areas/Dashboard/Controllers/KegiatanController.cs
+++ webSITE/webSITE/Areas/Dashboard/Controllers/KegiatanController.cs
@@ -9,17 +9,20 @@ namespace webSITE.Areas.Dashboard.Controllers
     public class KegiatanController : Controller
     {
         private readonly IRepositoriKegiatan _repositoriKegiatan;
-        private readonly IRepositoriPesertaKegiatan _pesertaKegiatan;
+        private readonly IRepositoriPesertaKegiatan _repositoriPesertaKegiatan;
         private readonly IRepositoriMahasiswa _repositoriMahasiswa;
+        private readonly IRepositoriFoto _repositoriFoto;
 
         public KegiatanController(
-            IRepositoriKegiatan repositoriKegiatan, 
-            IRepositoriPesertaKegiatan pesertaKegiatan, 
-            IRepositoriMahasiswa repositoriMahasiswa)
+            IRepositoriKegiatan repositoriKegiatan,
+            IRepositoriPesertaKegiatan pesertaKegiatan,
+            IRepositoriMahasiswa repositoriMahasiswa,
+            IRepositoriFoto repositoriFoto)
         {
             _repositoriKegiatan = repositoriKegiatan;
-            _pesertaKegiatan = pesertaKegiatan;
+            _repositoriPesertaKegiatan = pesertaKegiatan;
             _repositoriMahasiswa = repositoriMahasiswa;
+            _repositoriFoto = repositoriFoto;
         }
 
         public async Task<IActionResult> Index()
@@ -27,6 +30,20 @@ namespace webSITE.Areas.Dashboard.Controllers
             var listKegiatan = (await _repositoriKegiatan.GetAll()).ToList();
 
             return View(listKegiatan);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var kegiatan = await _repositoriKegiatan.GetWithDetail(id);
+
+            return View(kegiatan);
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _repositoriKegiatan.Delete(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
