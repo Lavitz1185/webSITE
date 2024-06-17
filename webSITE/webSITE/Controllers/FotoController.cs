2a50dbe4565a8220711dcca66c60b517ade4efd8
AdiJT <aditaklal@gmail.com>
2024-06-17 16:50:34 +0800

    Buang tambah foto dari halaman depan

--- webSITE/webSITE/Controllers/FotoController.cs
+++ webSITE/webSITE/Controllers/FotoController.cs
@@ -190,182 +190,182 @@ namespace webSITE.Controllers
             return View(viewModel);
         }
 
-        [HttpGet]
-        [Authorize]
-        public async Task<IActionResult> Tambah(int? idKegiatan)
-        {
-            var listMahasiswa = (await _repositoriMahasiswa.GetAll())
-                .Select(m => new MahasiswaTambahFotoVM
-                {
-                    Id = m.Id,
-                    NamaLengkap = m.NamaLengkap,
-                    DalamFoto = false
-                }).ToArray();
+        //[HttpGet]
+        //[Authorize]
+        //public async Task<IActionResult> Tambah(int? idKegiatan)
+        //{
+        //    var listMahasiswa = (await _repositoriMahasiswa.GetAll())
+        //        .Select(m => new MahasiswaTambahFotoVM
+        //        {
+        //            Id = m.Id,
+        //            NamaLengkap = m.NamaLengkap,
+        //            DalamFoto = false
+        //        }).ToArray();
 
-            return View(new TambahFotoVM
-            {
-                Tanggal = DateTime.Now,
-                DaftarMahasiswaTambahFotoVM = listMahasiswa,
-                IdKegiatan = idKegiatan
-            });
-        }
+        //    return View(new TambahFotoVM
+        //    {
+        //        Tanggal = DateTime.Now,
+        //        DaftarMahasiswaTambahFotoVM = listMahasiswa,
+        //        IdKegiatan = idKegiatan
+        //    });
+        //}
 
-        [HttpPost]
-        public async Task<IActionResult> Tambah(TambahFotoVM tambahFotoVM)
-        {
-            if (!ModelState.IsValid)
-            {
-                ModelState.AddModelError(string.Empty, "Perbaiki form!");
-                return View(tambahFotoVM);
-            }
+        //[HttpPost]
+        //public async Task<IActionResult> Tambah(TambahFotoVM tambahFotoVM)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        ModelState.AddModelError(string.Empty, "Perbaiki form!");
+        //        return View(tambahFotoVM);
+        //    }
 
-            var formFileContent = await FileHelpers.ProcessFormFile<TambahFotoVM>(
-                tambahFotoVM.FotoFormFile,
-                ModelState,
-                _photoFileSettings.PermittedExtension,
-                _photoFileSettings.FileSizeLimit);
+        //    var formFileContent = await FileHelpers.ProcessFormFile<TambahFotoVM>(
+        //        tambahFotoVM.FotoFormFile,
+        //        ModelState,
+        //        _photoFileSettings.PermittedExtension,
+        //        _photoFileSettings.FileSizeLimit);
 
-            if (!ModelState.IsValid)
-            {
-                return View(tambahFotoVM);
-            }
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(tambahFotoVM);
+        //    }
 
-            var trustedFileNameForFileStorage = $"{Path.GetRandomFileName()}{Guid.NewGuid()}{Path.GetExtension(tambahFotoVM.FotoFormFile.FileName)}";
-            var filePath = Path.Combine(
-                _photoFileSettings.StoredFilesPath, trustedFileNameForFileStorage);
+        //    var trustedFileNameForFileStorage = $"{Path.GetRandomFileName()}{Guid.NewGuid()}{Path.GetExtension(tambahFotoVM.FotoFormFile.FileName)}";
+        //    var filePath = Path.Combine(
+        //        _photoFileSettings.StoredFilesPath, trustedFileNameForFileStorage);
 
-            Foto newFoto = _mapper.Map<Foto>(tambahFotoVM);
-            newFoto.PhotoPath = filePath;
+        //    Foto newFoto = _mapper.Map<Foto>(tambahFotoVM);
+        //    newFoto.PhotoPath = filePath;
 
-            var idMahasiswaDalamFoto = tambahFotoVM.DaftarMahasiswaTambahFotoVM
-                .Where(x => x.DalamFoto == true)
-                .Select(x => x.Id).ToList();
+        //    var idMahasiswaDalamFoto = tambahFotoVM.DaftarMahasiswaTambahFotoVM
+        //        .Where(x => x.DalamFoto == true)
+        //        .Select(x => x.Id).ToList();
 
-            newFoto = await _repositoriFoto.Create(newFoto);
+        //    newFoto = await _repositoriFoto.Create(newFoto);
 
-            if (newFoto == null)
-            {
-                ModelState.AddModelError(string.Empty, "Error menambahkan foto");
-                return View(tambahFotoVM);
-            }
+        //    if (newFoto == null)
+        //    {
+        //        ModelState.AddModelError(string.Empty, "Error menambahkan foto");
+        //        return View(tambahFotoVM);
+        //    }
 
-            using (var fileStream = System.IO.File.Create(_webHostEnvironment.WebRootPath + filePath))
-            {
-                await fileStream.WriteAsync(formFileContent);
-            }
+        //    using (var fileStream = System.IO.File.Create(_webHostEnvironment.WebRootPath + filePath))
+        //    {
+        //        await fileStream.WriteAsync(formFileContent);
+        //    }
 
-            foreach (var id in idMahasiswaDalamFoto)
-                await _repositoriMahasiswaFoto.Create(id, newFoto.Id);
+        //    foreach (var id in idMahasiswaDalamFoto)
+        //        await _repositoriMahasiswaFoto.Create(id, newFoto.Id);
 
-            _notificationService.AddNotification(
-                new ToastrNotification
-                {
-                    Type = ToastrNotificationType.Success,
-                    Title = "Tambah Foto Sukses",
-                    Message = "Foto berhasil ditambahkan"
-                });
+        //    _notificationService.AddNotification(
+        //        new ToastrNotification
+        //        {
+        //            Type = ToastrNotificationType.Success,
+        //            Title = "Tambah Foto Sukses",
+        //            Message = "Foto berhasil ditambahkan"
+        //        });
 
-            return RedirectToAction(nameof(Album), new {idKegiatan = tambahFotoVM.IdKegiatan});
-        }
+        //    return RedirectToAction(nameof(Album), new {idKegiatan = tambahFotoVM.IdKegiatan});
+        //}
 
-        [HttpPost]
-        public async Task<IActionResult> PindahFoto(int? idKegiatan, int idFoto)
-        {
-            var returnUrl = Url.Action(nameof(Album), new { idKegiatan }) ?? "/";
-            string namaAlbum = "Lain - Lain";
+        //[HttpPost]
+        //public async Task<IActionResult> PindahFoto(int? idKegiatan, int idFoto)
+        //{
+        //    var returnUrl = Url.Action(nameof(Album), new { idKegiatan }) ?? "/";
+        //    string namaAlbum = "Lain - Lain";
 
-            var foto = await _repositoriFoto.Get(idFoto);
-            if(foto == null)
-            {
-                _notificationService.AddNotification(new ToastrNotification
-                {
-                    Type = ToastrNotificationType.Error,
-                    Title = "Pindah Foto Gagal",
-                    Message = $"Foto dengan id {idFoto} tidak ada"
-                });
+        //    var foto = await _repositoriFoto.Get(idFoto);
+        //    if(foto == null)
+        //    {
+        //        _notificationService.AddNotification(new ToastrNotification
+        //        {
+        //            Type = ToastrNotificationType.Error,
+        //            Title = "Pindah Foto Gagal",
+        //            Message = $"Foto dengan id {idFoto} tidak ada"
+        //        });
 
-                return Redirect(returnUrl);
-            }
+        //        return Redirect(returnUrl);
+        //    }
 
-            if(idKegiatan != null)
-            {
-                var kegiatan = await _repositoriKegiatan.Get(idKegiatan.Value);
-                if(kegiatan == null)
-                {
-                    _notificationService.AddNotification(new ToastrNotification
-                    {
-                        Type = ToastrNotificationType.Error,
-                        Title = "Pindah Foto Gagal",
-                        Message = $"Kegiatan dengan id {idKegiatan} tidak ada"
-                    });
+        //    if(idKegiatan != null)
+        //    {
+        //        var kegiatan = await _repositoriKegiatan.Get(idKegiatan.Value);
+        //        if(kegiatan == null)
+        //        {
+        //            _notificationService.AddNotification(new ToastrNotification
+        //            {
+        //                Type = ToastrNotificationType.Error,
+        //                Title = "Pindah Foto Gagal",
+        //                Message = $"Kegiatan dengan id {idKegiatan} tidak ada"
+        //            });
 
-                    return Redirect(returnUrl);
-                }
-                namaAlbum = kegiatan.NamaKegiatan;
-            }
+        //            return Redirect(returnUrl);
+        //        }
+        //        namaAlbum = kegiatan.NamaKegiatan;
+        //    }
 
-            if(foto.IdKegiatan == idKegiatan)
-            {
-                _notificationService.AddNotification(new ToastrNotification
-                {
-                    Type = ToastrNotificationType.Error,
-                    Title = "Pindah Foto Gagal",
-                    Message = $"Foto sudah ada di kegiatan {namaAlbum}"
-                });
+        //    if(foto.IdKegiatan == idKegiatan)
+        //    {
+        //        _notificationService.AddNotification(new ToastrNotification
+        //        {
+        //            Type = ToastrNotificationType.Error,
+        //            Title = "Pindah Foto Gagal",
+        //            Message = $"Foto sudah ada di kegiatan {namaAlbum}"
+        //        });
 
-                return Redirect(returnUrl);
-            }
+        //        return Redirect(returnUrl);
+        //    }
 
-            foto.IdKegiatan = idKegiatan;
-            var result = await _repositoriFoto.Update(foto);
+        //    foto.IdKegiatan = idKegiatan;
+        //    var result = await _repositoriFoto.Update(foto);
 
-            if (result == null)
-                _notificationService.AddNotification(new ToastrNotification
-                {
-                    Type = ToastrNotificationType.Error,
-                    Title = "Pindah Foto Gagal",
-                    Message = "Terjadi error pada server"
-                });
-            else
-                _notificationService.AddNotification(new ToastrNotification
-                {
-                    Type = ToastrNotificationType.Success,
-                    Title = "Pindah Foto Berhasil",
-                    Message = $"Foto dengan id {idFoto} berhasil dipindah ke kegiatan {namaAlbum}"
-                });
+        //    if (result == null)
+        //        _notificationService.AddNotification(new ToastrNotification
+        //        {
+        //            Type = ToastrNotificationType.Error,
+        //            Title = "Pindah Foto Gagal",
+        //            Message = "Terjadi error pada server"
+        //        });
+        //    else
+        //        _notificationService.AddNotification(new ToastrNotification
+        //        {
+        //            Type = ToastrNotificationType.Success,
+        //            Title = "Pindah Foto Berhasil",
+        //            Message = $"Foto dengan id {idFoto} berhasil dipindah ke kegiatan {namaAlbum}"
+        //        });
 
-            return Redirect(returnUrl);
-        }
+        //    return Redirect(returnUrl);
+        //}
 
-        [Authorize]
-        [HttpPost]
-        public async Task<IActionResult> Delete(int id, string? returnUrl)
-        {
-            returnUrl = returnUrl ?? Url.Action("Index");
+        //[Authorize]
+        //[HttpPost]
+        //public async Task<IActionResult> Delete(int id, string? returnUrl)
+        //{
+        //    returnUrl = returnUrl ?? Url.Action("Index");
 
-            var result = await _repositoriFoto.Delete(id);
+        //    var result = await _repositoriFoto.Delete(id);
 
-            System.IO.File.Delete(_webHostEnvironment.WebRootPath + result.PhotoPath);
+        //    System.IO.File.Delete(_webHostEnvironment.WebRootPath + result.PhotoPath);
 
-            if (result == null)
-                _notificationService.AddNotification(
-                    new ToastrNotification
-                    {
-                        Type = ToastrNotificationType.Error,
-                        Title = "Hapus Foto Gagal",
-                        Message = $"Foto dengan id {id} gagal dihapus"
-                    });
-            else
-                _notificationService.AddNotification(
-                    new ToastrNotification
-                    {
-                        Type = ToastrNotificationType.Success,
-                        Title = "Hapus Foto Sukses",
-                        Message = $"Foto dengan id {id} berhasil dihapus"
-                    });
+        //    if (result == null)
+        //        _notificationService.AddNotification(
+        //            new ToastrNotification
+        //            {
+        //                Type = ToastrNotificationType.Error,
+        //                Title = "Hapus Foto Gagal",
+        //                Message = $"Foto dengan id {id} gagal dihapus"
+        //            });
+        //    else
+        //        _notificationService.AddNotification(
+        //            new ToastrNotification
+        //            {
+        //                Type = ToastrNotificationType.Success,
+        //                Title = "Hapus Foto Sukses",
+        //                Message = $"Foto dengan id {id} berhasil dihapus"
+        //            });
 
 
-            return Redirect(returnUrl);
-        }
+        //    return Redirect(returnUrl);
+        //}
     }
 }
