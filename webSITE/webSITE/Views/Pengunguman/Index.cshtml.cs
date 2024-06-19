20ccb97f42fa09ec17811f8c12d440cbd4fde7c4
putra <fernandputra14@gmail.com>
2024-06-19 08:56:30 +0800

    git restore .

---
+++ webSITE/webSITE/Views/Pengunguman/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webSITE.Views.Pengunguman
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
