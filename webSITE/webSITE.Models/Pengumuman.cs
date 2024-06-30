fb53f406e172221bf9fefb122fa30651cab8e58f
AdiJT <aditaklal@gmail.com>
2024-06-30 23:08:54 +0800

    Tambah Pengumuman dan edit ukuran maksimal upload foto

---
+++ webSITE/webSITE.Models/Pengumuman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webSITE.Domain
+{
+    public class Pengumuman
+    {
+        public int Id { get; set; }
+        public string Judul { get; set; }
+        public string Isi { get; set; }
+        public byte[] Foto { get; set; }
+        public DateTime Tanggal { get; set; }
+        public bool IsPriority { get; set; }
+    }
+}
