7c6c81d8ea3291479ec237a453637b33bc10aad6
Lavitzz <allgaming1185@gmail.com>
2024-07-21 14:21:05 +0800

    update pop up

---
+++ webSITE/webSITE/wwwroot/js/fancybox.js
@@ -0,0 +1,27 @@
+﻿
+$(document).ready(function () {
+    $("[data-fancybox]").fancybox({
+        Image: {
+            initialSize: "fit",
+        },
+        Thumbs: {
+            showOnStart: true,
+            position: "bottom"
+        },
+        Toolbar: {
+            display: {
+                left: ["infobar"],
+                middle: [
+                    "zoomIn",
+                    "zoomOut",
+                    "toggle1to1",
+                    "rotateCCW",
+                    "rotateCW",
+                    "flipX",
+                    "flipY"
+                ],
+                right: ["slideshow", "thumbs", "close"]
+            }
+        }
+    });
+});
