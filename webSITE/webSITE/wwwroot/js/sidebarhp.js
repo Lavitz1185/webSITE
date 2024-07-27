bbbaefeda5024a36a725b90759507f7650bd9f64
putra <fernandputra14@gmail.com>
2024-07-27 19:09:07 +0800

    Update Dashboard Responsive

---
+++ webSITE/webSITE/wwwroot/js/sidebarhp.js
@@ -0,0 +1,14 @@
+﻿const toggleProfile = document.querySelector('.toggle-sidebar-hp');
+const showsidebar = document.querySelector('#sidebar');
+
+toggleProfile.addEventListener('click', function () {
+    showsidebar.classList.toggle('show');
+});
+
+// Menutup sidebar jika klik di luar sidebar
+document.addEventListener('click', function (event) {
+    // Cek jika sidebar terbuka dan klik di luar sidebar dan tombol toggle
+    if (showsidebar.classList.contains('show') && !showsidebar.contains(event.target) && !toggleProfile.contains(event.target)) {
+        showsidebar.classList.remove('show');
+    }
+});
