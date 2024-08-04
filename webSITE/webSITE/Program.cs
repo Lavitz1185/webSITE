a5dd35a1d7296f544e54704452f72b54cf401980
AdiJT <aditaklal@gmail.com>
2024-08-04 12:32:43 +0800

    Ubah sedikit

--- webSITE/webSITE/Program.cs
+++ webSITE/webSITE/Program.cs
@@ -74,16 +74,18 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseForwardedHeaders(new ForwardedHeadersOptions
+    {
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+    });
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
     app.UseHttpsRedirection();
 }
-
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-});
 
 app.UseStaticFiles();
 app.UseRouting();
