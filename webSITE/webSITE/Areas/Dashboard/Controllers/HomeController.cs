cce8e6fdcccb89544329b73cadd2bad29340fc67
AdiJT <aditaklal@gmail.com>
2024-08-08 10:53:05 +0800

    Selesai buat statistik dashboard

--- webSITE/webSITE/Areas/Dashboard/Controllers/HomeController.cs
+++ webSITE/webSITE/Areas/Dashboard/Controllers/HomeController.cs
@@ -1,16 +1,54 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using webSITE.Areas.Dashboard.Models.HomeController;
+using webSITE.DataAccess.Repositori.Interface;
+using webSITE.Domain.Enum;
 
 namespace webSITE.Areas.Dashboard.Controllers
 {
     [Area("Dashboard")]
     public class HomeController : Controller
     {
-        [Authorize(Roles = "Admin, ADMIN")]
-        public IActionResult Index()
+        private readonly IRepositoriMahasiswa _repositoriMahasiswa;
+        private readonly IRepositoriKegiatan _repositoriKegiatan;
+        private readonly IRepositoriFoto _repositoriFoto;
+        private readonly IRepositoriPengumuman _repositoriPengumuman;
+
+        public HomeController(
+            IRepositoriMahasiswa repositoriMahasiswa,
+            IRepositoriKegiatan repositoriKegiatan,
+            IRepositoriFoto repositoriFoto,
+            IRepositoriPengumuman repositoriPengumuman)
         {
-            return View();
+            _repositoriMahasiswa = repositoriMahasiswa;
+            _repositoriKegiatan = repositoriKegiatan;
+            _repositoriFoto = repositoriFoto;
+            _repositoriPengumuman = repositoriPengumuman;
+        }
+
+        [Authorize(Roles = "Admin, ADMIN")]
+        public async Task<IActionResult> Index()
+        {
+            var daftarMahasiswa = await _repositoriMahasiswa.GetAll();
+            var daftarKegiatan = await _repositoriKegiatan.GetAll();
+            var daftarFoto = await _repositoriFoto.GetAll();
+            var daftarPengumuman = await _repositoriPengumuman.GetAll();
+
+            daftarMahasiswa = daftarMahasiswa ?? new();
+            daftarKegiatan = daftarKegiatan ?? new();
+            daftarFoto = daftarFoto ?? new();
+            daftarPengumuman = daftarPengumuman ?? new();
+
+            return View(new IndexVM
+            {
+                TotalMahasiswa = daftarMahasiswa.Count,
+                TotalMahasiswaPria = daftarMahasiswa.Where(m => m.JenisKelamin == JenisKelamin.LakiLaki).Count(),
+                TotalMahasiswaWanita = daftarMahasiswa.Where(m => m.JenisKelamin == JenisKelamin.Perempuan).Count(),
+                JumlahFoto = daftarFoto.Count,
+                JumlahKegiatan = daftarKegiatan.Count,
+                JumlahPengumuman = daftarPengumuman.Count
+            });
         }
     }
 }
---
+++ webSITE/webSITE/Areas/Dashboard/Models/HomeController/IndexVM.cs
@@ -0,0 +1,11 @@
+﻿namespace webSITE.Areas.Dashboard.Models.HomeController;
+
+public class IndexVM
+{
+    public int TotalMahasiswa { get; set; }
+    public int TotalMahasiswaPria { get; set; }
+    public int TotalMahasiswaWanita { get; set; }
+    public int JumlahKegiatan { get; set; }
+    public int JumlahFoto { get; set; }
+    public int JumlahPengumuman { get; set; }
+}
